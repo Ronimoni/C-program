97b0bce7e4e4395b23583a2cef63a3247a1d4eef
RONIMONI <appsroni@gmail.com>
2017-07-21 18:38:01 +0600

    malti array input output

---
+++ 2D Array and there Sum.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    system("color bc");
+   int i,j,k,l,sum=0;
+   int array[4][5];
+   for(i=0;i<4;i++){
+       for(j=0;j<5;j++){
+
+           scanf("%d",&array[i][j]);
+
+           }
+       }
+ for(k=0;k<4;k++){
+       for(l=0;l<5;l++){
+
+             sum=sum+l+1;
+
+              printf("%d ",sum);
+
+           }
+
+       }
+}
